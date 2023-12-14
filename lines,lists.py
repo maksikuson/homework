b53986c8581ebe08254af7bd1c56a53e1d9a7132
Your Name <you@example.com>
2023-12-14 22:09:54 +0200

    зробив перше завдання

--- lines,lists.py
+++ lines,lists.py
@@ -1,1 +1,14 @@
+#Part 1
+
+#Task 1
+
+def palindrome(i):
+    i = i.replace(" ", "").lower()
+    return i == i[::-1]
+
+line = input("Enter line: ")
+if palindrome(line):
+    print("It's a palindrome!")
+else:
+    print("This is not a palindrome(.")
 
