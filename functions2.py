36dffc654a04abed79e0d19602aed83b9bd26635
Your Name <you@example.com>
2023-12-29 19:10:29 +0200

    зробив до 3 завдання

--- functions2.py
+++ functions2.py
@@ -1,1 +1,35 @@
+#Part 1
+
+#Task 1
+
+#Я не знаю як робити перше завдання.
+
+#Task 2
+
+def EvenNums(a, d):
+    for i in range(a, d):
+        if i % 2 == 0:
+            print(i)
+
+EvenNums(1, 38)
+
+#Task 3
+
+def rooms(leng, symbols, operation):
+    string = ""
+    if operation == True:
+        for i in range(leng):
+            string +=  symbols + " "
+        for i in range(leng):
+            print(string)
+    if operation == False:
+        for i in range(leng):
+            string += symbols + ' '
+        print(string)
+        for i in range(leng - 6):
+            print( symbols + " " * (leng + (leng - 4)) + symbols)
+        print(string)   
+rooms(6, "=", False)
+rooms(6, "=", True)
+
 
