06388622b7fb6b20256000a914d287c6a8e360eb
Your Name <you@example.com>
2024-01-20 15:15:01 +0200

    зробив перше

--- Module6.Functions.py
+++ Module6.Functions.py
@@ -1,1 +1,14 @@
+#Task 1
 
+def recursive(a, b):
+    if b == 0:
+        return a
+    else:
+        return recursive(b, a % b)
+num1 = 88
+num2 = 66
+result = recursive(num1, num2)
+
+print(f"The greatest common divisor of {num1} and {num2} is: {result}")
+
+#Task 2
