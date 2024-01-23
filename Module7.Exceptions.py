31e142511a935b33b0710d11672727ee33cb4259
Your Name <you@example.com>
2024-01-23 18:21:31 +0200

    зробив перше завдання

--- Module7.Exceptions.py
+++ Module7.Exceptions.py
@@ -1,1 +1,13 @@
+#Task 1
 
+try:
+    name = str(input("Enter your name : "))
+    age = int(input("Enter your age : "))
+    if 0 < age < 130:
+        print(f"Hello, {name}! Your age - {age}")
+    else:
+        raise ValueError('Incorrect age')
+except ValueError as e:
+    print(f'ERROR: {e}')
+    
+#Task 2
