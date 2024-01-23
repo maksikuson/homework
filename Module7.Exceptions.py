166ac2e49e1b982a960d7175a0826bce0fded40e
Your Name <you@example.com>
2024-01-23 18:47:56 +0200

    зробив друге завдання

--- Module7.Exceptions.py
+++ Module7.Exceptions.py
@@ -11,3 +11,37 @@ except ValueError as e:
     print(f'ERROR: {e}')
     
 #Task 2
+
+def userInfo(name, age):
+    if 0 < age < 130:
+        return f"Hello, {name}! Your age - {age}."
+    else:
+        raise ValueError('Incorrect age')
+try:
+    user_name = input("Enter your name: ")
+    user_age = int(input("Enter your age: "))
+    result = userInfo(user_name, user_age)
+    print(result)
+except ValueError as e:
+    print(f'ERROR: {e}')
+    
+    
+def userInfo(name, age):
+    try:
+        if 0 < age < 130:
+            return f"Hello, {name}! Your age - {age}."
+        else:
+            raise ValueError('Incorrect age')
+    except ValueError as e:
+        return f'ERROR: {e}'
+    
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: "))
+
+try:
+    user_age = int(user_age)
+    result = userInfo(user_name, user_age)
+    print(result)
+except ValueError as e:
+    print(f'ERROR: {e}')
+    
