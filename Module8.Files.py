e361685885eac6f1c6e2f607f9b1bcde6efe3a74
Your Name <you@example.com>
2024-02-03 17:37:04 +0200

    зробив друге

--- Module8.Files.py
+++ Module8.Files.py
@@ -22,3 +22,25 @@ if differences2:
     print(f"Lines that do not match in {file2_path}:\n{differences2}")
 else:
     print(f"All lines in {file2_path} match.")
+    
+#Task 2
+
+with open('file1.txt', 'r',encoding='utf-8') as file1:
+    numberOfFiles = file1.read()
+    symbol = len(numberOfFiles)
+    lines = len(numberOfFiles.splitlines())
+    
+    vomels = 'aeiou'
+    vomelsCount = sum(char.lower() in vomels for char in numberOfFiles)
+    
+    consonants = 'bcdfghjklmnpqrstvwxyz'
+    consonantCount = sum(char.lower() in consonants for char in numberOfFiles)
+    
+    digits = sum(char.isdigit() for char in numberOfFiles)
+    
+
+print(f"Number of symbols: {symbol}")
+print(f"Number of lines: {lines}")
+print(f"Number of vowels: {vomelsCount}")
+print(f"Number of consonant letters: {consonantCount}")
+print(f"Number of digits: {digits}")
