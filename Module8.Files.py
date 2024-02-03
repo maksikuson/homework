c8d29dbf0299ef537e258f6d70048a8a088f6bdc
Your Name <you@example.com>
2024-02-03 16:45:32 +0200

    почав робити перше завдання

--- Module8.Files.py
+++ Module8.Files.py
@@ -1,1 +1,7 @@
+file_path = "file1.txt"
+file2_path = "file2.txt"
 
+with open(file_path, 'r', encoding='uth-8') as file1, open(file2_path, 'r', encoding='uth-8') as file2:
+     lines1 = file1.readlines()
+     lines2 = file2.readlines()
+     
